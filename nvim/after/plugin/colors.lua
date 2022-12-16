139b6bf4d5e9b9b0636d400fdc64004c30091314
Boran Seckin <boran@boranseckin.com>
2022-12-16 14:09:12 -0500

    Add neovim config

---
+++ nvim/after/plugin/colors.lua
@@ -0,0 +1,10 @@
+function FixColors(color)
+  color = color or "duskfox"
+  vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+FixColors()
+
---
+++ nvim/lua/boran/init.lua
@@ -0,0 +1,3 @@
+require("boran.set")
+require("boran.remap")
+
