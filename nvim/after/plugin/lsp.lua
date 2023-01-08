424cccfee67457fa3147e20eb156cbe30317a926
Boran Seckin <boran@boranseckin.com>
2023-01-08 15:44:20 -0500

    Update LSP sign icons

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -9,6 +9,16 @@ lsp.ensure_installed({
   'rust_analyzer',
 })
 
+lsp.set_preferences({
+  suggest_lsp_servers = false,
+  sign_icons = {
+    error = 'E',
+    warn = 'W',
+    hint = 'H',
+    info = 'I'
+  }
+})
+
 lsp.setup()
 
 vim.diagnostic.config({
