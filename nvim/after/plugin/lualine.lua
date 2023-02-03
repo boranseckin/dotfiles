9dd67cb960b7b5daf6f33e6aaac2d78869ae89e3
Boran Seckin <boran@boranseckin.com>
2023-02-03 14:18:20 -0500

    Configure lualine

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,5 +1,29 @@
 require('lualine').setup({
   options = {
     theme = 'material',
-  }
+  },
+  extensions = {
+    "fzf",
+    "toggleterm"
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch' },
+    lualine_c = {
+      {
+        'filename',
+        file_status = true,
+        newfile_status = true,
+        path = 1
+      },
+      {
+        'diagnostics',
+        sources = { 'nvim_diagnostic', 'nvim_lsp' },
+      },
+    },
+    lualine_x = { 'encoding', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' },
+  },
 })
+
