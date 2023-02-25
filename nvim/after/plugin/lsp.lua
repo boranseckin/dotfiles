d65c5f753def08d771971fe0bb065d2f816107d0
Boran Seckin <boran@boranseckin.com>
2023-02-25 14:41:20 -0500

    Refactor lsp and copilot mappings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,11 +1,11 @@
 local lsp = require('lsp-zero')
+local cmp = require('cmp')
 
 lsp.preset('recommended')
 
 lsp.ensure_installed({
   'tsserver',
   'eslint',
-  'sumneko_lua',
   'rust_analyzer',
 })
 
@@ -16,12 +16,34 @@ lsp.set_preferences({
     warn = 'W',
     hint = 'H',
     info = 'I'
+  },
+  set_lsp_keymaps = { omit = { '<F2>', '<F4>' } }
+})
+
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr}
+  local bind = vim.keymap.set
+
+  bind('n', 'gr', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+  bind('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+end)
+
+lsp.setup_nvim_cmp({
+  mapping = lsp.defaults.cmp_mappings({
+    ['<C-y>'] = cmp.mapping(function(fallback)
+      vim.api.nvim_feedkeys(vim.fn['copilot#Accept'](vim.api.nvim_replace_termcodes('<Tab>', true, true, true)), 'n', true)
+    end)
+  }),
+  experimental = {
+    ghost_text = false
   }
 })
+
+lsp.nvim_workspace()
 
 lsp.setup()
 
 vim.diagnostic.config({
   virtual_text = true,
-  globals = { 'vim' },
 })
+
