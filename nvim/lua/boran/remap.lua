023fce2b9f72872323e9a148ac399237fae10f31
Boran Seckin <boran@boranseckin.com>
2023-04-23 15:06:57 -0400

    Add move line remap

--- nvim/lua/boran/remap.lua
+++ nvim/lua/boran/remap.lua
@@ -1,6 +1,14 @@
 vim.g.mapleader = " ";
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex);
 vim.keymap.set("n", "<leader>w", vim.cmd.w);
+
+-- Escape insert mode
 vim.keymap.set("i", "jj", "<ESC>");
 vim.keymap.set("i", "kk", "<ESC>");
+
+-- Escape terminal mode
 vim.keymap.set("t", "<ESC>", "<C-\\><C-n>");
+
+-- Move selected line / block of text in visual mode
+vim.keymap.set("v", "<S-Up>", ":m '<-2<CR>gv=gv");
+vim.keymap.set("v", "<S-Down>", ":m '>+1<CR>gv=gv");
