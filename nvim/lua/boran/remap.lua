b2a42ffe57c2f752ce817ff4a83d35592b5fe99f
Boran Seckin <boran@boranseckin.com>
2023-04-23 16:11:36 -0400

    Add clipboard remaps

--- nvim/lua/boran/remap.lua
+++ nvim/lua/boran/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " ";
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex);
+vim.keymap.set("n", "<leader>ex", vim.cmd.Ex);
 vim.keymap.set("n", "<leader>w", vim.cmd.w);
 
 -- Escape insert mode
@@ -12,3 +12,9 @@ vim.keymap.set("t", "<ESC>", "<C-\\><C-n>");
 -- Move selected line / block of text in visual mode
 vim.keymap.set("v", "<S-Up>", ":m '<-2<CR>gv=gv");
 vim.keymap.set("v", "<S-Down>", ":m '>+1<CR>gv=gv");
+
+-- Copy/Paste from/to system clipboard
+vim.keymap.set("n", "<leader>y", '"+y');
+vim.keymap.set("v", "<leader>y", '"+y');
+vim.keymap.set("n", "<leader>p", '"+p');
+vim.keymap.set("v", "<leader>p", '"+p');
