414a41e3a0e6e3521da907e69c6703a7df4231b2
Boran Seckin <boran@boranseckin.com>
2023-10-21 12:21:26 -0400

    feat: add word jumping

--- wezterm.lua
+++ wezterm.lua
@@ -20,6 +20,10 @@ config.quick_select_patterns = {
 
 config.keys = {
   { key = 't', mods = 'CMD|SHIFT', action = wezterm.action.ShowLauncher },
+  -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+  {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+  -- Make Option-Right equivalent to Alt-f; forward-word
+  {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
 }
 
 return config
