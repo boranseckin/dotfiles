61849bdde1944c5f80227c640386fa2392d952e8
Boran Seckin <boran@boranseckin.com>
2024-01-28 12:23:38 -0500

    feat: unmap arrow keys

--- nvim/lua/boran/remap.lua
+++ nvim/lua/boran/remap.lua
@@ -6,6 +6,8 @@ vim.keymap.set("n", "<leader> ", "<C-^>")
 -- Escape insert mode
 vim.keymap.set("i", "jj", "<ESC>");
 vim.keymap.set("i", "kk", "<ESC>");
+vim.keymap.set("i", "jk", "<ESC>");
+vim.keymap.set("i", "kj", "<ESC>");
 
 -- Escape terminal mode
 vim.keymap.set("t", "<ESC>", "<C-\\><C-n>");
@@ -15,7 +17,11 @@ vim.keymap.set("v", "<S-Up>", ":m '<-2<CR>gv=gv");
 vim.keymap.set("v", "<S-Down>", ":m '>+1<CR>gv=gv");
 
 -- Copy/Paste from/to system clipboard
-vim.keymap.set("n", "<leader>y", '"+y');
-vim.keymap.set("v", "<leader>y", '"+y');
-vim.keymap.set("n", "<leader>p", '"+p');
-vim.keymap.set("v", "<leader>p", '"+p');
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y');
+vim.keymap.set({ "n", "v" }, "<leader>p", '"+p');
+
+-- Unbind arrow keys
+vim.keymap.set({ "n", "v", "i" }, "<Up>", "<Nop>");
+vim.keymap.set({ "n", "v", "i" }, "<Down>", "<Nop>");
+vim.keymap.set({ "n", "v", "i" }, "<Left>", "<Nop>");
+vim.keymap.set({ "n", "v", "i" }, "<Right>", "<Nop>");
