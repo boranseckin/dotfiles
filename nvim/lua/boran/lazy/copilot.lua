905be270a29cd58d940e7b405bb75a8ebeb97251
Boran Seckin <boran@boranseckin.com>
2024-05-16 15:52:42 -0400

    feat: add copilot

---
+++ nvim/lua/boran/lazy/copilot.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = false,
+        keymap = {
+          accept = "<C-y>",
+          next = "<C-u>",
+        }
+      },
+      panel = { enabled = false },
+    },
+  },
+}
