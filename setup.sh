2ab4c83ccbf8d5ad7aa6ce9f04bc123e056e5c64
Boran Seckin <boran@boranseckin.com>
2025-01-24 11:22:31 -0500

    feat: add fontconfig

--- setup.sh
+++ setup.sh
@@ -4,12 +4,13 @@ link() {
     SRC="$PWD/$1"
     DST=$2
     #mkdir -p ${2%/*}
-    ln -sf $SRC $DST
+    ln -sfn $SRC $DST
     echo "$1 -> $2"
 }
 
 link hyperzsh.zsh-theme $ZSH/custom/themes/hyperzsh.zsh-theme
 link zshrc $HOME/.zshrc
+link fontconfig $XDG_CONFIG_HOME/fontconfig
 link git $XDG_CONFIG_HOME/git
 link tmux $XDG_CONFIG_HOME/tmux
 link nvim $XDG_CONFIG_HOME/nvim
