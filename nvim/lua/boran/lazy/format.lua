1a59c50da61c8e6efb0efbfda5d902821e458d9e
Boran Seckin <boran@boranseckin.com>
2025-02-06 15:06:33 -0500

    feat: refactor formatter to use conform

---
+++ nvim/lua/boran/lazy/format.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    'stevearc/conform.nvim',
+    opts = {},
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          python = { "ruff_fix", "ruff_format" },
+          c = { "clang-format" },
+          json = { "biome" },
+          jsonc = { "biome" },
+          markdown = { "markdownlint" },
+        },
+        default_format_opts = {
+          lsp_format = "fallback",
+        },
+        format_on_save = {
+          lsp_format = "fallback",
+          timeout_ms = 500,
+        }
+      })
+    end,
+  },
+}
