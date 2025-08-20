f9c02621cbf5e74ea2f5d15290e37d7cbfea5b32
Boran Seckin <boran@boranseckin.com>
2025-08-20 11:58:46 -0400

    feat(arch): add obsidian wayland flags

--- setup.sh
+++ setup.sh
@@ -21,3 +21,4 @@ link tmux $XDG_CONFIG_HOME/tmux
 link nvim $XDG_CONFIG_HOME/nvim
 link logiops $XDG_CONFIG_HOME/logiops
 link dunst $XDG_CONFIG_HOME/dunst
+link obsidian/user-flags.conf $XDG_CONFIG_HOME/obsidian/user-flags.conf
