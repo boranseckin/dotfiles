d5c4d6a2a856e0b377f2edaced017645ec54b9db
Boran Seckin <boran@boranseckin.com>
2026-03-31 16:52:56 -0400

    fix: shellcheck setup.sh

--- setup.sh
+++ setup.sh
@@ -3,21 +3,21 @@
 link() {
   SRC="$PWD/$1"
   DST=$2
-  #mkdir -p ${2%/*}
-  ln -sfn $SRC $DST
+  mkdir -p "${2%/*}"
+  ln -sfn "$SRC" "$DST"
   echo "$1 -> $2"
 }
 
-link custom.zsh-theme $ZSH/custom/themes/custom.zsh-theme
-link zshrc $HOME/.zshrc
-link fontconfig $XDG_CONFIG_HOME/fontconfig
-link hypr $XDG_CONFIG_HOME/hypr
-link waybar $XDG_CONFIG_HOME/waybar
-link wofi $XDG_CONFIG_HOME/wofi
-link kitty $XDG_CONFIG_HOME/kitty
-link git $XDG_CONFIG_HOME/git
-link tmux $XDG_CONFIG_HOME/tmux
-link nvim $XDG_CONFIG_HOME/nvim
-link logiops $XDG_CONFIG_HOME/logiops
-link dunst $XDG_CONFIG_HOME/dunst
-link obsidian/user-flags.conf $XDG_CONFIG_HOME/obsidian/user-flags.conf
+link custom.zsh-theme "$ZSH"/custom/themes/custom.zsh-theme
+link zshrc "$HOME"/.zshrc
+link fontconfig "$XDG_CONFIG_HOME"/fontconfig
+link hypr "$XDG_CONFIG_HOME"/hypr
+link waybar "$XDG_CONFIG_HOME"/waybar
+link wofi "$XDG_CONFIG_HOME"/wofi
+link kitty "$XDG_CONFIG_HOME"/kitty
+link git "$XDG_CONFIG_HOME"/git
+link tmux "$XDG_CONFIG_HOME"/tmux
+link nvim "$XDG_CONFIG_HOME"/nvim
+link logiops "$XDG_CONFIG_HOME"/logiops
+link dunst "$XDG_CONFIG_HOME"/dunst
+link obsidian/user-flags.conf "$XDG_CONFIG_HOME"/obsidian/user-flags.conf
