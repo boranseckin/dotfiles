dfa1c22285a5f91d0a709a3b74bed85fe138cb87
Boran Seckin <boran@boranseckin.com>
2026-03-31 16:39:45 -0400

    fix: remove old link

--- setup.sh
+++ setup.sh
@@ -14,7 +14,6 @@ link fontconfig $XDG_CONFIG_HOME/fontconfig
 link hypr $XDG_CONFIG_HOME/hypr
 link waybar $XDG_CONFIG_HOME/waybar
 link wofi $XDG_CONFIG_HOME/wofi
-link ghostty $XDG_CONFIG_HOME/ghostty
 link kitty $XDG_CONFIG_HOME/kitty
 link git $XDG_CONFIG_HOME/git
 link tmux $XDG_CONFIG_HOME/tmux
