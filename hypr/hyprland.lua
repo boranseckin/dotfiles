73c238ae5f842fe6ea89c6fba972b0415a6624b6
Boran Seckin <boran@boranseckin.com>
2026-08-29 21:46:51 -0400

    fix(arch): gtk and qt styling

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -29,9 +29,9 @@ local bluetooth   = "/bin/bash ~/.config/hypr/scripts/bluetooth.sh"
 -------------------
 
 hl.on("hyprland.start", function()
+  hl.exec_cmd("gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'")
   hl.exec_cmd("uwsm app -- quickshell")
   hl.exec_cmd("uwsm app -- copyq --start-server")
-  hl.exec_cmd("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'")
 end)
 
 -------------------------------
